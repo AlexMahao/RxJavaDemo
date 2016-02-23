3dc58d8933eb9adf1d6a78670b1777ed986ce352
Alex_Mahao <zziamahao@163.com>
2016-02-23 16:17:25 +0800

    one

---
+++ app/src/main/java/com/example/mdw/rxjavademo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mdw.rxjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        findViewById(R.id.load).startAnimation(anim);
+
+    }
+}
